0e2d9a8575971a59c9fa4e59acba77519a713c55
agustinbarrera99 <agustinbarrera.ri@gmail.com>
2026-04-16 09:55:35 -0300

    Creacio de base de datos

--- PryBarreraSP1ER/ClaseEspecialidad.cs
+++ PryBarreraSP1ER/ClaseEspecialidad.cs
@@ -4,19 +4,19 @@ namespace PryBarreraSP1ER
 {
     public class ClaseEspecialidad
     {
-        public int Numero { get; set; }
+        public int IdEspecialidad { get; set; }
         public string Nombre { get; set; }
 
         public ClaseEspecialidad()
         {
         }
 
-        public ClaseEspecialidad(int numero, string nombre)
+        public ClaseEspecialidad(int idEspecialidad, string nombre)
         {
-            if (numero <= 0) throw new ArgumentException("El número de especialidad debe ser un valor positivo.", nameof(numero));
+            if (idEspecialidad <= 0) throw new ArgumentException("El número de especialidad debe ser un valor positivo.", nameof(numero));
             if (string.IsNullOrWhiteSpace(nombre)) throw new ArgumentException("El nombre de la especialidad no puede estar vacío.", nameof(nombre));
 
-            Numero = numero;
+            IdEspecialidad = idEspecialidad;
             Nombre = nombre.Trim();
         }
 
